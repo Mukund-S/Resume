7843ccead877441c3efeec01a91edd186b9fef06
Mukund-S <61332056+Mukund-S@users.noreply.github.com>
2023-10-06 12:10:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Backend Engineer"],
+        strings: ["Software Developer", "Backend Engineer", "DevOps Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Backend Engineer"],
+        strings: ["Software Developer", "Backend Engineer", "DevOps Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
